b77741cbc9394f6125fc91172b79e69586abc4f2
Edge <dcc@monika.love>
2026-02-16 21:51:04 +0800

    non-utf8 crash fix

--- src/Formatter/TextPreview/FormatTextPreview.php
+++ src/Formatter/TextPreview/FormatTextPreview.php
@@ -44,7 +44,7 @@ class FormatTextPreview
 
             if ($file) {
                 $file_contents = file_get_contents($this->paths->public.'/assets/files/'.$file->path);
-                $lines = mb_split('\R', $file_contents);
+                $lines = preg_split('/\R/', $file_contents);
                 $lines = array_filter($lines, 'trim');
 
                 if (count($lines) <= 5) {
