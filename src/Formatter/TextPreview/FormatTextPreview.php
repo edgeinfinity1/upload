ca0bf0e4850d61ad3d8e181737f316660ba4f1c5
Edge <dcc@monika.love>
2026-02-19 19:53:39 +0800

    fix

--- src/Formatter/TextPreview/FormatTextPreview.php
+++ src/Formatter/TextPreview/FormatTextPreview.php
@@ -44,7 +44,7 @@ class FormatTextPreview
 
             if ($file) {
                 $file_contents = file_get_contents($this->paths->public.'/assets/files/'.$file->path);
-                $lines = preg_split('/\R/', $file_contents);
+                $lines = preg_split('/\R/u', $file_contents);
                 $lines = array_filter($lines, 'trim');
 
                 if (count($lines) <= 5) {
